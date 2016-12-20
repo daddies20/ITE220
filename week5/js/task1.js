2c0dd7e10f0754fcb5f69473536ebe1c9d41d80e
daddies20 <daddies20@yahoo.com>
2016-12-21 03:35:16 +1100

    Add files via upload

---
+++ week5/js/task1.js
@@ -0,0 +1,25 @@
+(function(){
+
+var task1 = {
+	render: function(){
+		document.getElementById("screenX").textContent = window.screenX;
+		document.getElementById("screenY").textContent = window.screenY;
+		document.getElementById("screenWidth").textContent = window.innerWidth;
+    document.getElementById("screenHeight").textContent = window.innerHeight;
+
+    document.getElementById("location").textContent = window.location.pathname;
+	},
+  resize: function(){
+    window.addEventListener("resize", this.render);
+  },
+  devInfo: function () {
+    var btnDev = document.getElementById("dev-info");
+    btnDev.addEventListener("click", function(){
+      document.getElementById("dev-info-container").innerHTML = "<p>Chidi Kingsley<br>IT Student<br><a href='https://github.com/daddies20' target='_blank'>Github Link</a></p>"
+      });
+  }
+}
+task1.render();
+task1.resize();
+task1.devInfo();
+})();
